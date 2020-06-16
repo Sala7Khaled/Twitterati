95a2110b0b3afd8e80152493a80342c0d962c487
S7K <sala7khaled.s7k@gmail.com>
2020-06-16 20:21:30 +0200

    Initial Commit

---
+++ Twitterati/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twitterati
+//
+//  Created by Salah Khaled on 6/16/20.
+//  Copyright © 2020 Salah Khaled. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
